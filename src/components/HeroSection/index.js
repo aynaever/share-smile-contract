63f921fafebf48445934d844afda3225142767ad
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-10 00:42:20 +0100

    add HeroSection component

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,13 @@
+import CallToAction from "./CallToAction/CallToAction";
+import Heading from "./Heading/Heading";
+
+function HeroSection() {
+	return (
+		<>
+			<Heading />
+			<CallToAction />
+		</>
+	);
+}
+
+export default HeroSection;
