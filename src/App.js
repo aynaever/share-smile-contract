5822c3d8941f7593ba75142163866c9a3f25e2c9
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-10 14:21:35 +0100

    add chakra ui to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar/Navbar';
+import HeroSection from './components/HeroSection';
+import MissionSection from './components/MissionSection/MissionSection';
+import FormSection from './components/FormSection/FormSection';
+import FooterSection from './components/FooterSection/FooterSection';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	  <>
+  	  	<Navbar />
+  	  	<HeroSection />
+  		<MissionSection />
+  	  	<FormSection />
+  	  	<FooterSection />
+  	  </>
   );
 }
 
