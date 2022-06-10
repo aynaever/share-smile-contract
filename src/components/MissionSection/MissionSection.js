152d76f0fbd0f5700dad6146e99e5d2cb38dca71
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-10 15:10:34 +0100

    add some content and styling to OurMission section

--- src/components/MissionSection/MissionSection.js
+++ src/components/MissionSection/MissionSection.js
@@ -1,8 +1,20 @@
+import { Heading, Image } from "@chakra-ui/react";
+import MissionSectionCss from "./MissionSection.css";
+
 function MissionSection() {
 	return (
-		<>
-			<h1>Mission Section Here</h1>
-		</>
+		<div className="missionSection">
+			<Heading>
+				What is our <span id="mission">Mission</span> ?
+			</Heading>
+			<Heading>
+				We want to paint Happy
+				<br />Smiles on the Faces of
+				<br />Everyone
+				<br />#EveryoneDeservesSmile
+			</Heading>
+			<Image src="/OurMission.png" />
+		</div>
 	);
 }
 
