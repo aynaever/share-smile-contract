32fc3c0ce4785ebef7843edb13b0a90e8fd220d1
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-10 20:21:59 +0100

    add some motions to HeroSection

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,20 +1,35 @@
 import { Heading as HeadingChakra} from '@chakra-ui/react';
 import { Image, Button } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import IndexCss from "./index.css";
 
 function HeroSection() {
 	return (
 		<div className="heroSection">
-			<div className="division">
-				<HeadingChakra as="h1" fontSize='5rem' noOfLines={4} size="3xl" className="slang">
+			<motion.div
+				animate={{x: 100, scale: 1}}
+				transition={{ type: "tween", duration: 2}}
+				initial={{ scale: 0}}
+				className="division">
+				<HeadingChakra
+					as="h1"
+					fontSize='5rem'
+					noOfLines={4} size="3xl"
+					className="slang">
 					Share <HeadingChakra className="love" fontSize='8rem' size="4xl" as="h1">Love</HeadingChakra>
 					<br />Now in the <br />
 					Blockchain
 					<br />
 				</HeadingChakra>
 				<Button className="cta" colorScheme="orange" size="lg">Sm😁le Now</Button>
-			</div>
-			<Image src="/HeroImage.png" className="image"/>
+			</motion.div>
+			<motion.div
+				animate={{ scale : 1, rotate: 0}}
+				transition={{ type: "tween", duration: 1}}
+				initial={{ scale : 2 }}
+				className="image">
+				<Image src="/HeroImage.png" className="image"/>
+			</motion.div>
 		</div>
 	);
 }
