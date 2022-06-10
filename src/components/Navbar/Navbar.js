ff13f4a792fa8b55ba50b1c7bc412ed8273b74e8
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-10 13:03:16 +0100

    add links and logo to Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,14 @@
 function Navbar() {
 	return (
 		<>
-			<h1>Navbar</h1>
+			<ul>
+				<li>Home</li>
+				<li>Smiles</li>
+				<li><button>Smile Now</button></li>
+				<li>About</li>
+			</ul>
+			<img src="/Logo.png" />
+			<button>Connect</button>
 		</>
 	);
 }
