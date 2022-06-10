fdc39875264093378bb490ceebed6013e4a3455d
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-10 15:50:04 +0100

    add some content and styling to FormSection

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -1,8 +1,25 @@
+import { Heading,
+		 FormControl,
+		 Input,
+		 Textarea,
+		 Button } from "@chakra-ui/react";
+import FormSectionCss from "./FormSection.css";
+
 function FormSection() {
 	return (
-		<>
-			<h1>Form Section</h1>
-		</>
+		<div className="formSection">
+			<Heading id="title">
+				One <span id="click">Click </span>
+				One <span id="smile">Smile</span>
+			</Heading>
+			<div className="blockForm">
+				<FormControl>
+					<Input type="text" placeHolder="Your name" />
+					<Textarea />
+					<Button>Smile </Button>
+				</FormControl>
+			</div>
+		</div>
 	);
 }
 
