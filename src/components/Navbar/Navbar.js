33775aad8b996109f6fd44b5513171b179b8f7c4
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-10 14:21:02 +0100

    add some styling to navbar and chakra ui elements

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,18 @@
+import NavbarCss from './Navbar.css';
+import { Button, Link } from '@chakra-ui/react';
+
 function Navbar() {
 	return (
-		<>
+		<div className="navbar">
 			<ul>
-				<li>Home</li>
-				<li>Smiles</li>
-				<li><button>Smile Now</button></li>
-				<li>About</li>
+				<Link>Home</Link>
+				<Link>Smiles</Link>
+				<Button colorScheme="orange" size="md">Smile Now</Button>
+				<Link>About</Link>
 			</ul>
 			<img src="/Logo.png" />
-			<button>Connect</button>
-		</>
+			<Button className="button" colorScheme="orange" size="md">Connect</Button>
+		</div>
 	);
 }
 
