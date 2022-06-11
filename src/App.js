278f51c7b80c4348eeb6455d92b19798fe3af054
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-11 19:34:24 +0100

    add routes and router to the application and route content depends on the url

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import HeroSection from './components/HeroSection';
 import MissionSection from './components/MissionSection/MissionSection';
 import FormSection from './components/FormSection/FormSection';
 import FooterSection from './components/FooterSection/FooterSection';
-import { BrowserRouter } from "react-router-dom";
+import About from './components/AboutSection/AboutSection';
+import SmilesSection from './components/SmilesSeciton/SmilesSection';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
   	  <BrowserRouter>
   	  	<Navbar />
-  	  	<HeroSection />
-  		<MissionSection />
-  	  	<FormSection />
+		<Routes>
+			<Route path="/" element={<HeroSection /> }/>
+			<Route path="/smiles" element={<SmilesSection /> }/>
+			<Route path="/smile-now" element={<FormSection /> }/>
+			<Route path="/about" element={<About /> }/>
+  	  	</Routes>
   	  	<FooterSection />
   	  </BrowserRouter>
   );
