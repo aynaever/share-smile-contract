ea12f2213e15e918ad6cfaffc594b90f209fed54
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-11 19:16:49 +0100

    add react-router-dom to the project and control the browser url and the links in navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import HeroSection from './components/HeroSection';
 import MissionSection from './components/MissionSection/MissionSection';
 import FormSection from './components/FormSection/FormSection';
 import FooterSection from './components/FooterSection/FooterSection';
+import { BrowserRouter } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-  	  <>
+  	  <BrowserRouter>
   	  	<Navbar />
   	  	<HeroSection />
   		<MissionSection />
   	  	<FormSection />
   	  	<FooterSection />
-  	  </>
+  	  </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import NavbarCss from './Navbar.css';
 import { connectToEthereum, isWalletConnected } from '../../utils/connectToEthereum.js';
 import { motion } from "framer-motion";
 import { Button, Link } from '@chakra-ui/react';
+import { Link as RouterLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Navbar() {
@@ -11,10 +12,12 @@ function Navbar() {
 	return (
 		<div className="navbar">
 			<ul>
-				<Link>Home</Link>
-				<Link>Smiles</Link>
-				<Button colorScheme="orange" size="md">Smile Now</Button>
-				<Link>About</Link>
+				<Link as={RouterLink} to="/">Home</Link>
+				<Link as={RouterLink} to="/smiles">Smiles</Link>
+				<Link as={RouterLink} to="/smile-now">
+					<Button colorScheme="orange" size="md">Smile Now</Button>
+				</Link>
+				<Link as={RouterLink} to="/about">About</Link>
 			</ul>
 			<img src="/Logo.png" />
 			<motion.div className="button" whileHover={{ scale: 1.1 }}>
