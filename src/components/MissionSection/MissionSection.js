c001543bd935e061af22a1b529a1e6382e3e3532
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-12 16:58:04 +0100

    Add animations and transitions to OurMission section (about page)

--- src/components/MissionSection/MissionSection.js
+++ src/components/MissionSection/MissionSection.js
@@ -5,18 +5,32 @@ import MissionSectionCss from "./MissionSection.css";
 function MissionSection() {
 	return (
 		<div className="missionSection">
-			<Heading	className="headingMission"
-						fontSize='3rem'
-						as='h1'>
-				What is our <span id="mission">Mission</span> ?
-			</Heading>
-			<Heading className="slang" fontSize='3rem'>
-				We want to paint <span className="happySmiles">Happy
-				<br />Smiles</span> on the Faces of
-				<br />Everyone
-				<br /><span className="hashtag">#EveryoneDeservesSmile</span>
-			</Heading>
-			<Image className="image" src="/OurMission.png" />
+			<motion.div	className="headingMission"
+						initial={{ opacity : 0 }}
+						animate={{ y : [-50, 0], scale : 1.1, opacity : [0.2, 0.5, 1] }}
+						transition={{ ease : "easeOut", duration : 1.5 }}>
+				<Heading	fontSize='3rem'
+							as='h1'>
+					What is our <span id="mission">Mission</span> ?
+				</Heading>
+			</motion.div>
+			<motion.div	className="slang"
+						initial={{ opacity : 0 }}
+						animate={{ x : [50, 0], scale : 1.1, opacity : [0.2, 0.5, 1] }}
+						transition={{ ease : "easeOut", duration : 2.5 }}>
+				<Heading className="slang" fontSize='3rem'>
+					We want to paint <span className="happySmiles">Happy
+					<br />Smiles</span> on the Faces of
+					<br />Everyone
+					<br /><span className="hashtag">#EveryoneDeservesSmile</span>
+				</Heading>
+			</motion.div>
+			<motion.div	className="image"
+						initial={{ opacity : 0 }}
+						animate={{ x : [-50, 0], scale : 1, opacity : [0.2, 0.5, 1] }}
+						transition={{ ease : "easeOut", duration : 2.5 }}>
+				<Image className="image" src="/OurMission.png" />
+			</motion.div>
 		</div>
 	);
 }
