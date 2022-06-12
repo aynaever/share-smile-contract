da8330d8bac38873f45018f361100ec6f3a61ae6
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-12 20:12:54 +0100

    show smiles and messages and names in smiles section

--- src/components/SmilesSeciton/SmilesSection.js
+++ src/components/SmilesSeciton/SmilesSection.js
@@ -5,13 +5,27 @@ import SmilesSectionCss from "./SmilesSection.css";
 
 function SmilesSection() {
 	const [nsmiles, setNsmiles] = useState();
+	const [smiles, setSmiles] = useState([]);
+	const divSmiles = [];
+
+	smiles.forEach( smile => {
+		divSmiles.push(
+				<div>
+					<Heading as={'h2'}>
+						{smile.name.length === 0 ? "Anonym" : smile.name}
+					</Heading>
+					<Heading as={'h3'}>
+						{smile.message.length === 0 ? "No message" : smile.message}
+					</Heading>
+				</div>);
+	});
 
 	useEffect(() => {
 			(async () => {
 				const num = await getTotalSmiles();
-				await getSmiles();
+				setSmiles(await getSmiles());
 				setNsmiles(num)})();
-	});
+	}, []);
 
 	return (
 		<div className="smilesSection">
@@ -21,6 +35,7 @@ function SmilesSection() {
 							noOfLines={2}>
 						Happy Smiles from Happy People</Heading>
 				<Heading>{nsmiles}</Heading>
+				{divSmiles}
 			</Container>
 		</div>
 	);
