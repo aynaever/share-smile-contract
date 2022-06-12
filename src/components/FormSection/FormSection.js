c8399049c25b5c8a476631dfd071ccefe62e83df
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-12 14:13:40 +0100

    Add animations to the form section

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -5,6 +5,7 @@ import { Heading,
 		 Button } from "@chakra-ui/react";
 import { useState } from "react";
 import { sendSmile } from "../../utils/connectToEthereum";
+import { motion } from "framer-motion";
 import FormSectionCss from "./FormSection.css";
 
 function FormSection() {
@@ -13,11 +14,18 @@ function FormSection() {
 
 	return (
 		<div className="formSection">
-			<Heading id="title" size="3xl">
-				One <span id="click">Click </span>
-				One <span id="smile">Smile</span>
-			</Heading>
-			<div className="blockForm">
+			<motion.div	initial={{ opacity : 0 }}
+						animate={{ y : [0, 50], scale : 1.1, opacity : [0.2, 0.5, 1] }}
+						transition={{ ease : "easeOut", duration : 1.5 }}>
+					<Heading id="title" size="3xl">
+					One <span id="click">Click </span>
+					One <span id="smile">Smile</span>
+				</Heading>
+			</motion.div>
+			<motion.div	className="blockForm"
+						initial={{ opacity : 0 }}
+						animate={{ y : [50, 0], scale : 1.1, opacity : [0.2, 0.5, 1] }}
+						transition={{ ease : "easeOut", duration : 1.5 }}>
 				<FormControl>
 					<Input	type="text"
 							placeHolder="Your name"
@@ -28,10 +36,13 @@ function FormSection() {
 								placeHolder="Write your happy message here to the Blockchain"
 								className="messageInput"
 								onChange={(e) => setMessage(e.target.value)}/>
+					<motion.button	whileHover={{ scale : 1.2 }}
+									whileTap={{ scale : 0.9 }}>
 					<Button	colorScheme="blue"
 							onClick={() => sendSmile(name, message)}>Smile </Button>
+					</motion.button>
 				</FormControl>
-			</div>
+			</motion.div>
 		</div>
 	);
 }
