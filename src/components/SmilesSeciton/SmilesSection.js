289e8994b0365f4a16c90f42039e1d7434b437f2
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-13 00:04:21 +0100

    add motions to smiles section and fix some styling

--- src/components/SmilesSeciton/SmilesSection.js
+++ src/components/SmilesSeciton/SmilesSection.js
@@ -1,25 +1,35 @@
 import { Container, Heading, Button, Box } from "@chakra-ui/react";
 import { getTotalSmiles, getSmiles } from "../../utils/connectToEthereum";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import SmilesSectionCss from "./SmilesSection.css";
 
 function Smiles(props) {
 	const smiley = props.smiley;
+	const sign = props.index % 2 === 0 ? 1 : -1;
 
 	return (
-		<Box	borderRadius='lg'
-				borderWidth='1px'
-				bg='orange.200'
-				mb={5}>
-				<Heading	as={'h2'}
-							fontSize={40}>
-					 {smiley.name.length === 0 ? "Anonym" : smiley.name}
-				</Heading>
-				<Heading	as={'h5'}
-							fontSize={20}>
-					{smiley.message.length === 0 ? "No message" : smiley.message}
-				</Heading>
-		</Box>
+		<motion.div	whileHover={{ scale : 1.2 }}
+					initial={{ opacity : 0 }}
+					animate={{ x : [sign * 100, 0], opacity : 1, duration : 2 }}
+					>
+			<Box	borderRadius='lg'
+					borderWidth='1px'
+					bg='orange.200'
+					p={3}
+					mb={5}>
+					<Heading	as={'h2'}
+								fontWeight={40}
+								fontSize={40}>
+					 	 {smiley.name.length === 0 ? "Anonym" : smiley.name}
+					</Heading>
+					<Heading	as={'h5'}
+								fontWeight={12}
+								fontSize={17}>
+						{smiley.message.length === 0 ? "No message" : smiley.message}
+					</Heading>
+			</Box>
+		</motion.div>
 	);
 }
 
@@ -28,9 +38,9 @@ function SmilesSection() {
 	const [smiles, setSmiles] = useState([]);
 	const divSmiles = [];
 
-	smiles.forEach( smile => {
+	smiles.forEach( (smile, index) => {
 		divSmiles.push(
-				<Smiles smiley={smile}>
+				<Smiles smiley={smile} index={index}>
 				</Smiles>);
 	});
 
@@ -43,12 +53,15 @@ function SmilesSection() {
 
 	return (
 		<div className="smilesSection">
-			<Container maxW="4xl">
+			<Container maxW="6xl">
 				<Heading	pt={20}
+							fontSize={50}
 							textAlign={'center'}
+							mb={20}
 							noOfLines={2}>
-						Happy Smiles from Happy People</Heading>
-				<Heading>{nsmiles}</Heading>
+					<span className="nSmiles">{nsmiles} </span>
+					Happy <span className="smiles">Smiles </span>
+					from <span className="happy">Happy</span> People</Heading>
 				{divSmiles}
 			</Container>
 		</div>
