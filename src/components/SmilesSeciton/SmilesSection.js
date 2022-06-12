cd7f77d35c4499efc4470211ca05bb394fec5c43
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-12 20:37:12 +0100

    add stylign to smiles section

--- src/components/SmilesSeciton/SmilesSection.js
+++ src/components/SmilesSeciton/SmilesSection.js
@@ -1,7 +1,27 @@
-import { Container, Heading, Button } from "@chakra-ui/react";
+import { Container, Heading, Button, Box } from "@chakra-ui/react";
 import { getTotalSmiles, getSmiles } from "../../utils/connectToEthereum";
 import { useEffect, useState } from "react";
 import SmilesSectionCss from "./SmilesSection.css";
+
+function Smiles(props) {
+	const smiley = props.smiley;
+
+	return (
+		<Box	borderRadius='lg'
+				borderWidth='1px'
+				bg='orange.200'
+				mb={5}>
+				<Heading	as={'h2'}
+							fontSize={40}>
+					 {smiley.name.length === 0 ? "Anonym" : smiley.name}
+				</Heading>
+				<Heading	as={'h5'}
+							fontSize={20}>
+					{smiley.message.length === 0 ? "No message" : smiley.message}
+				</Heading>
+		</Box>
+	);
+}
 
 function SmilesSection() {
 	const [nsmiles, setNsmiles] = useState();
@@ -10,14 +30,8 @@ function SmilesSection() {
 
 	smiles.forEach( smile => {
 		divSmiles.push(
-				<div>
-					<Heading as={'h2'}>
-						{smile.name.length === 0 ? "Anonym" : smile.name}
-					</Heading>
-					<Heading as={'h3'}>
-						{smile.message.length === 0 ? "No message" : smile.message}
-					</Heading>
-				</div>);
+				<Smiles smiley={smile}>
+				</Smiles>);
 	});
 
 	useEffect(() => {
