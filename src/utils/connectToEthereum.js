611c5d97029442e0dc12f8539789310e626aa862
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-06-13 21:20:37 +0100

    update smart contract address

--- src/utils/connectToEthereum.js
+++ src/utils/connectToEthereum.js
@@ -1,6 +1,8 @@
 import { ethers, providers } from "ethers";
 import abi from "../ShareSmile.json";
 
+const address = "0xCecF7AfC5C5f91073F70FCe82c3A7975FF05d517";
+
 export async function connectToEthereum() {
 	const { ethereum } = window;
 
@@ -18,7 +20,7 @@ export async function sendSmile(name, message) {
 	if (!(await isWalletConnected()))
 		await connectToEthereum();
 	try {
-		const addressContract = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+		const addressContract = address;
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		const signer = provider.getSigner();
 		const contract = new ethers.Contract(addressContract, abi.abi, signer);
@@ -34,7 +36,7 @@ export async function getTotalSmiles() {
 		connectToEthereum();
 
 	try {
-		const addressContract = "0xCecF7AfC5C5f91073F70FCe82c3A7975FF05d517";
+		const addressContract = address;
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		const signer = provider.getSigner();
 		const contract = new ethers.Contract(addressContract, abi.abi, signer);
@@ -48,7 +50,7 @@ export async function getTotalSmiles() {
 
 export async function getSmiles() {
 	try {
-		const addressContract = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+		const addressContract = address;
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		const contract = new ethers.Contract(addressContract, abi.abi, provider);
 		let smiles = await contract.getSmiles();
